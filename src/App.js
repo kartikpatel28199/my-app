5ed74e0d1ae3b1ed6893a029b7e4fb164744104e
Your Name <karishma@ladyboss.com>
2021-09-09 21:57:38 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Route, Switch } from "react-router-dom";
+import MainNavigation from "./components/layout/MainNavigation";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavouritesPage from "./pages/Favourites";
+import NewMeetupPage from "./pages/NewMeetups";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <MainNavigation />
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetups">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favourites">
+          <FavouritesPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
